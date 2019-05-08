e5a7a66463c3bdc05d206a3a95b2178130c711e1
Ariana Melis <arianamelis@gmail.com>
2019-05-08 03:50:44 -0700

    updates

--- client/src/components/LoginForm/index.js
+++ client/src/components/LoginForm/index.js
@@ -1,41 +1,42 @@
 import React from "react";
 
 import "../../Styles/app.css";
-function LoginForm (props) {
-    return (
-        <div>
-        <div className="card">
-          <h2>Log in </h2>
-          <h5>
-            Welcome back
-          </h5>
-  
-          <form
-            className="login"
-            action="/api/users"
-            class="col s12 z-depth-1"
-            method="POST"
-          >
-            <div class="row ">
-              <div class="input-field col s6">
-                <label for="email">E-mail:  </label>
-                <input id="email" type="text" class="validate" />
-              </div>
-            </div>
-            <div class="row">
-              <div class="input-field col s6">
-                <label for="password">Password:  </label>
-                <input id="password" type="text" class="validate" />
-              </div>
-            </div>
-            <button type="submit" onClick={props.handleFormSubmit} className="btn btn-info">
-          Go
-        </button>
-            </form>
-</div>
-</div>
+function LoginForm(props) {
+  return (
+    <div>
+      <div className="card">
+        <h2>Log in </h2>
+        <h5>Welcome back</h5>
 
-    );
-  }
-  
-  export default LoginForm;
+        <form
+          className="login"
+          action="/api/users"
+          class="col s12 z-depth-1"
+          method="POST"
+        >
+          <div class="row ">
+            <div class="input-field col s6">
+              <label for="username">E-mail: </label>
+              <input id="username" type="text" class="validate" />
+            </div>
+          </div>
+          <div class="row">
+            <div class="input-field col s6">
+              <label for="password">Password: </label>
+              <input id="password" type="text" class="validate" />
+            </div>
+          </div>
+          <button
+            type="submit"
+            onClick={props.handleFormSubmit}
+            className="btn btn-info"
+          >
+            Go
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default LoginForm;
