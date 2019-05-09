3be427167fde67c484b403bb545ba59b24745806
Ariana Melis <arianamelis@gmail.com>
2019-05-09 00:48:54 -0700

    working materialize carousel

--- client/src/pages/CarouselEx.js
+++ client/src/pages/CarouselEx.js
@@ -4,13 +4,15 @@ import Container from "../components/Container";
 import Header from "../components/Header";
 import Wrapper from "../components/Wrapper";
 import ControlledCarousel from "../components/Carousel";
-
+import RegContainer from "../components/RegContainer";
 
 function CarouselEx() {
     return (
       <div>
           <Wrapper>
+              <RegContainer>
                <ControlledCarousel />
+               </RegContainer>
                </Wrapper>    
           </div>
 
