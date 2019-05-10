38a91e7b126718f16e257da960167793c7e8ca4d
Ariana Melis <arianamelis@gmail.com>
2019-05-09 21:14:13 -0700

    pray

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -3,43 +3,6 @@ import Hero from "../components/Hero";
 import Container from "../components/Container";
 import GlobalCard from "../components/GlobalCard";
 import Header from "../components/Header";
-<<<<<<< HEAD
-import Wrapper from "../components/Wrapper";
-import LoginForm from "../components/LoginForm";
-
-function Login() {
-  return (
-    <div>
-      <Wrapper>
-      <Header backgroundImage="/assets/images/indiela.png" fluid>
-  </Header>
-        <LoginForm />
-
-      </Wrapper>
-    </div>
-  );
-}
-
-export default Login;
-import Wrapper from "../components/Wrapper";
-import LoginForm from "../components/LoginForm";
-
-function Login() {
-  return (
-    <div>
-      <Wrapper>
-      <Header backgroundImage="/assets/images/indiela.png" fluid>
-  </Header>
-        <LoginForm />
-
-      </Wrapper>
-    </div>
-  );
-}
-
-export default Login;
-=======
->>>>>>> 38022b564781f50c0f9c34439bd4114427725575
 import Wrapper from "../components/Wrapper";
 import LoginForm from "../components/LoginForm";
 
