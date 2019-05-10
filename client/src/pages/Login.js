c88a9944d85a88793f2da7ba13141b446504c22a
Ariana Melis <arianamelis@gmail.com>
2019-05-09 21:13:04 -0700

    Revert "updatez"
    This reverts commit c73fd43f2b88ded0505f8a6f0765be713898584c.

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -25,17 +25,32 @@ import LoginForm from "../components/LoginForm";
 
 function Login() {
   return (
-   
-      <div>
-        <Wrapper>
-        <Header backgroundImage="/assets/images/indiela.png" fluid>
-    </Header>
-          <LoginForm />
-  
-        </Wrapper>
-      </div>
-    );
-  }
-  
-  export default Login;
-  
+    <div>
+      <Wrapper>
+      <Header backgroundImage="/assets/images/indiela.png" fluid>
+  </Header>
+        <LoginForm />
+
+      </Wrapper>
+    </div>
+  );
+}
+
+export default Login;
+import Wrapper from "../components/Wrapper";
+import LoginForm from "../components/LoginForm";
+
+function Login() {
+  return (
+    <div>
+      <Wrapper>
+      <Header backgroundImage="/assets/images/indiela.png" fluid>
+  </Header>
+        <LoginForm />
+
+      </Wrapper>
+    </div>
+  );
+}
+
+export default Login;
