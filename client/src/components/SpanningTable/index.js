b1e080564d937510a1d1e9be232b92958ee73437
Ariana Melis <arianamelis@gmail.com>
2019-05-12 22:43:19 -0700

    formatted table

--- client/src/components/SpanningTable/index.js
+++ client/src/components/SpanningTable/index.js
@@ -37,9 +37,10 @@ function createRow(id, Artist, Date, Location, Time, Ticket) {
 
 
 const rows = [
-  ['Artist1', 100, 1.15],
-  ['Artist2', 10, 45.99],
-  ['Artist3', 2, 17.99],
+  ['Arctic Monkeys', '08/19', 'Hollywood Bowl', '9:00PM', 'link2tickets'],
+  ['Death Cab for Cutie', '08/11', 'Hollywood Bowl', '7:00PM', 'https://my.hollywoodbowl.com/syos/performance/3889?_ga=2.18352121.1982955710.1557725650-364484445.1557725650'],
+  ['Adam Melchor', '06/21', 'The Morrocan', '9:30PM', 'https://www.themoroccan.com/event/1833186-adam-melchor-los-angeles/'],
+  ['The Strokes', '05/13','The Western', '7:00 PM','https://concerts1.livenation.com/event/0900567C1'],
 ].map((row, id) => createRow(id, ...row));
 
 
@@ -62,10 +63,12 @@ function SpanningTable(props) {
         <TableBody>
           {rows.map(row => (
             <TableRow key={row.id}>
-              <TableCell>{row.desc}</TableCell>
-              <TableCell align="right">{row.qty}</TableCell>
-              <TableCell align="right">{row.unit}</TableCell>
-              </TableRow>
+              <TableCell>{row.Artist}</TableCell>
+              <TableCell align="right">{row.Date}</TableCell>
+              <TableCell align="right">{row.Location}</TableCell>
+              <TableCell align="right">{row.Time}</TableCell>
+              <TableCell align="right">{row.Ticket}</TableCell>
+            </TableRow>
           ))}
          
         </TableBody>
