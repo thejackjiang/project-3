3a5ba1e4da2eebbc5592e415e7efb6e9f8f40be6
Ariana Melis <arianamelis@gmail.com>
2019-05-12 22:06:32 -0700

    working spanningtable

---
+++ client/src/components/SpanningTable/index.js
@@ -0,0 +1,87 @@
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const TAX_RATE = 0.07;
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 7,
+    overflowX: 'auto',
+  },
+  table: {
+    minWidth: 700,
+    padding:10
+  },
+});
+
+function ccyFormat(num) {
+  return `${num.toFixed(2)}`;
+}
+
+function priceRow(qty, unit) {
+  return qty * unit;
+}
+
+function createRow(id, desc, qty, unit) {
+  const price = priceRow(qty, unit);
+  return { id, desc, qty, unit, price };
+}
+
+function subtotal(items) {
+  return items.map(({ price }) => price).reduce((sum, i) => sum + i, 0);
+}
+
+const rows = [
+  ['Artist1', 100, 1.15],
+  ['Artist2', 10, 45.99],
+  ['Artist3', 2, 17.99],
+].map((row, id) => createRow(id, ...row));
+
+const invoiceSubtotal = subtotal(rows);
+const invoiceTaxes = TAX_RATE * invoiceSubtotal;
+const invoiceTotal = invoiceTaxes + invoiceSubtotal;
+
+function SpanningTable(props) {
+  const { classes } = props;
+  return (
+    <Paper className={classes.root}>
+      <Table className={classes.table}>
+        <TableHead>
+          <TableRow>
+            <TableCell>Artist</TableCell>
+            <TableCell align="right">Date</TableCell>
+            <TableCell align="right">Location</TableCell>
+            <TableCell align="right">Time</TableCell>
+            <TableCell align="right">Ticket</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map(row => (
+            <TableRow key={row.id}>
+              <TableCell>{row.desc}</TableCell>
+              <TableCell align="right">{row.qty}</TableCell>
+              <TableCell align="right">{row.unit}</TableCell>
+              <TableCell align="right">{ccyFormat(row.price)}</TableCell>
+            </TableRow>
+          ))}
+         
+        </TableBody>
+      </Table>
+    </Paper>
+  );
+}
+
+SpanningTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(SpanningTable);
