cc2ee718782ece9ce8c73452266e993d6a980dac
Ariana Melis <arianamelis@gmail.com>
2019-05-18 10:36:48 -0700

    card updates

--- client/src/pages/Search.js
+++ client/src/pages/Search.js
@@ -3,6 +3,8 @@ import React from "react";
 import API from '../utils/API'
 import EventCard from '../components/EventCard/EventCard';
 import Wrapper from "../components/Wrapper";
+import List from "../components/List";
+import { Col, Row, Container } from "../components/Grid";
 export default class Search extends React.Component {
     constructor(props) {
         super(props)
@@ -21,16 +23,35 @@ export default class Search extends React.Component {
     render() {
         console.log(this.state)
         return (
-            <Wrapper>
-            <div>
+         
                
-                <h1>IndieEvents</h1>
+         
+                <Wrapper>
+                     
+              <Row>
+          <Col size="md-12">
+           
+              <h1 className="text-center">
+                <strong>Explore IndieEvents</strong>
+              </h1>
+              <h2 className="text-center">Search and save upcoming indie events</h2>
+     </Col>
+              </Row>
+              <Container>
+              <Row>
+           
+              <Col size="md-6">
                 {this.state.artists.data && this.state.artists.data.map(event => {
                     return <EventCard event={event}/>
                 })}
-
-            </div>
+ 
+       
+   </Col>
+   </Row>
+ 
+   </Container>          
             </Wrapper>
+  
         )
     }
 }
